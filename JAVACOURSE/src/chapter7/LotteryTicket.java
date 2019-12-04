a7cd04106681959fc3c1a1518e3fb5ce0f51dbb9
GiuliBentancor <54630519+GiuliBentancor@users.noreply.github.com>
2019-12-04 16:47:06 -0300

    Add files via upload

---
+++ JAVACOURSE/src/chapter7/LotteryTicket.java
@@ -0,0 +1,66 @@
+package chapter7;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LotteryTicket {
+
+    private static final int LENGTH = 6;// mayus variable constante final
+    private static final int MAX_TICKET_NUMBER = 69;
+    public static void main (String args []){
+        int [] ticket = generateNumbers();
+        Arrays.sort(ticket);
+        printTicket(ticket);
+    }
+
+    //[] return array
+    public static int[] generateNumbers(){
+        int[] ticket= new int[LENGTH];
+        Random random = new Random();
+
+        for (int i=0; i< LENGTH; i++){
+            int randomNumber;
+            do{
+                randomNumber= random.nextInt(MAX_TICKET_NUMBER) + 1;
+            } while (search(ticket, randomNumber));
+
+            ticket[i]= randomNumber;
+        }
+
+        return ticket;
+    }
+
+    /** JAVADOC
+     * Does a sequential search on the array
+     * @param array Array to search
+     * @param numberToSearchFor value to search for
+     * @return true if found, false if no
+     */
+     public static boolean search(int[] array, int numberToSearchFor){
+            //ENHACED LOOP - interates througt array
+         for(int value: array){
+             if (value == numberToSearchFor){
+                 return true;
+             }
+         }
+         return false;
+     }
+
+
+     public static boolean binarySearch(int [] array, int numberToSearchFor){
+         Arrays.sort(array);
+         int index = Arrays.binarySearch(array, numberToSearchFor);
+         if (index >=0){
+             return true;
+         } else return false;
+     }
+
+    public static void printTicket(int ticket[]) {
+        for (int i = 0; i < LENGTH; i++) {
+            System.out.print(ticket[i] + " | ");
+        }
+
+        //entire array search and value nt faund
+    }
+
+}
---
+++ JAVACOURSE/src/chapter7/chapter7.java
@@ -0,0 +1,32 @@
+package chapter7;
+
+/*
+
+int [] lottoTicket = new  int[6];
+
+int - la primera parte es el tipo de datos que contiene la matriz y aunque la matriz es
+ capaz de contener múltiples valores, todos los valores deben ser del mismo tipo de datos
+
+[]- los corchetes indican que este no es solo un inttipo de datos, sino que será
+una matriz de "entradas" . El paréntesis puede aparecer después del tipo de datos o
+después del nombre de la variable
+
+lottoTicket- Hablando de nombre, esa es la siguiente parte de la declaración.
+Nombramos matrices tal como lo haríamos con cualquier otra variable
+
+= - el nombre va seguido de un signo igual
+
+new- y luego usamos la newpalabra clave
+
+int - seguido por el tipo de datos nuevamente
+
+`[6] - seguido de un número dentro de los corchetes. Este número dentro de los corchetes representa la longitud de la matriz, lo que significa cuántos valores puede almacenar esta matriz. Una vez que se declara la longitud, se fija, lo que significa que no se puede alargar para contener más valores.
+
+or
+
+int[] lottoTicket = {24,18,9,11,34,64}
+
+
+ */
+public class chapter7 {
+}
